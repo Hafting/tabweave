92b7410816604cc8cc2dc15f8ef901b3a9e7f20b
Helge Hafting <helge.hafting@ntebb.no>
2025-09-03 16:03:54 +0200

    g.py file for testing gr graphics API

---
+++ g.py
@@ -0,0 +1,15 @@
+#For testing and figuring out gr
+import gr
+
+
+#gr.clearws()
+#gr.setviewport(0, 1, 0, 1)
+gr.setwindow(-5,5,-5,5)
+gr.fillarea([1,2,2],[1,2,1])
+gr.updatews()
+input("enter")
+gr.clearws()
+gr.setwindow(-3,3,-3,3)
+gr.fillarea([1,2,2],[1,2,1])
+gr.updatews()
+input("e")
